2ee2448e8e29f5e3a9dc95a9ddbd0945f98b3e16
mediocrity <kristiansson.tobias@gmail.com>
2014-02-14 14:48:01 +0100

    Factored out custom settings from init.el

--- init.el
+++ init.el
@@ -1,33 +1,18 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
- '(column-number-mode t)
- '(custom-enabled-themes (quote (tsdh-dark)))
- '(custom-safe-themes (quote ("f80f1e6ce6b9459f72caa92fa111abc629fa7c828617ba5d4c51f2b063c1a1eb" "d22444a942334d9b40baefefbd92644e4c9c9d0268f91dfc3aac7edb2f9cdea0" "e0bebff05b9b0bc478087dfe5173104f3ea105a767e9712d7fad3cc33c141c2f" "17d68ab9e3562df397e5f1837c1161473c74c78869547f27d810936b0ae2cb48" "fe0a47cc3952fede574527a1c28ddf3a1af381fc1fb5843ca60d22e4c841011a" default)))
- '(inhibit-startup-screen t)
- '(preview-gs-options (quote ("-q" "-dNOPAUSE" "-DNOPLATFONTS" "-dPrinted" "-dTextAlphaBits=4" "-dGraphicsAlphaBits=4")))
- '(scroll-bar-mode nil)
- '(show-paren-mode t)
- '(size-indication-mode t)
- '(tool-bar-mode nil)
- '(tooltip-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 113 :width normal)))))
+(column-number-mode t)
+(setq inhibit-startup-screen t)
+(scroll-bar-mode nil)
+(show-paren-mode t)
+(size-indication-mode t)
+(tool-bar-mode nil)
+(tooltip-mode nil)
+(menu-bar-mode -1)
+(setq visible-bell 1)
+
 
 (put 'narrow-to-region 'disabled nil)
 (setq org-src-fontify-natively t)
 (put 'dired-find-alternate-file 'disabled nil)
 
-(menu-bar-mode -1)
-(setq visible-bell 1)
- 
 
 ;; Backups and auto-saves
 (setq backup-directory-alist 
@@ -173,4 +158,9 @@
 
 
 ;; Loading init files
+(setq custom-file "~/.emacs.d/init_custom.el")
+(load custom-file)
+
 (load "~/.emacs.d/init_python.el")
+
+
---
+++ init_custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
+ '(custom-enabled-themes (quote (tsdh-dark)))
+ '(custom-safe-themes (quote ("f80f1e6ce6b9459f72caa92fa111abc629fa7c828617ba5d4c51f2b063c1a1eb" "d22444a942334d9b40baefefbd92644e4c9c9d0268f91dfc3aac7edb2f9cdea0" "e0bebff05b9b0bc478087dfe5173104f3ea105a767e9712d7fad3cc33c141c2f" "17d68ab9e3562df397e5f1837c1161473c74c78869547f27d810936b0ae2cb48" "fe0a47cc3952fede574527a1c28ddf3a1af381fc1fb5843ca60d22e4c841011a" default)))
+ '(preview-gs-options (quote ("-q" "-dNOPAUSE" "-DNOPLATFONTS" "-dPrinted" "-dTextAlphaBits=4" "-dGraphicsAlphaBits=4"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 113 :width normal)))))
