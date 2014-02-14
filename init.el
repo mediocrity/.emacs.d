ed1a8610bb5c9eeae3aa715d034f96e8137f116f
mediocrity <kristiansson.tobias@gmail.com>
2014-02-14 14:28:46 +0100

    Modified structure of init.el

--- init.el
+++ init.el
@@ -21,36 +21,19 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 113 :width normal)))))
 
-
-(menu-bar-mode -1)
-(setq visible-bell 1)
 (put 'narrow-to-region 'disabled nil)
 (setq org-src-fontify-natively t)
 (put 'dired-find-alternate-file 'disabled nil)
 
+(menu-bar-mode -1)
+(setq visible-bell 1)
+ 
 
-;; Loading Files
-(load "~/.emacs.d/init_python.el")
+;; Backups and auto-saves
+(setq backup-directory-alist 
+      `((".*" . ,"~/.saves")))
+(setq auto-save-default nil) ; I save too often anyway.
 
-
-(add-to-list 'load-path
-	     "~/.emacs.d/elpa/yasnippet-20140106.1009")
-(require 'yasnippet)
-(yas-global-mode 1)
-(setq yas/indent-line nil)
-
-
-;; Emacs modification
-(defun open-emacs-file (args):
-  (interactive "P")
-  (find-file "~/.emacs.d/init.el"))
-
-(global-set-key (kbd "C-<f1>") 'open-emacs-file)
-
-;; Flymake
-; http://stackoverflow.com/questions/11269937/syntax-highlighting-conflicts-with-flymake-errline-color-in-emacs
-(custom-set-faces '(flymake-errline ((((class color)) (:background "Black")))) 
-'(flymake-warnline ((((class color)) (:background "Gray50")))))
 
 ;; Melpa
 (when (>= emacs-major-version 24)
@@ -60,13 +43,34 @@
   )
 
 
-;; TEXT
-(add-hook 'text-mode-hook 'flyspell-mode)
-(add-hook 'text-mode-hook 'auto-fill-mode)
+
+;; IDO mode
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+;; yasnippet
+(add-to-list 'load-path
+	     "~/.emacs.d/elpa/yasnippet-20140106.1009")
+(require 'yasnippet)
+(yas-global-mode 1)
+(setq yas/indent-line nil)
+
+;; Flymake
+; http://stackoverflow.com/questions/11269937/syntax-highlighting-conflicts-with-flymake-errline-color-in-emacs
+(custom-set-faces '(flymake-errline ((((class color)) (:background "Black")))) 
+'(flymake-warnline ((((class color)) (:background "Gray50")))))
+
+(window-numbering-mode)
+
+(require 'move-text)
+(move-text-default-bindings)
 
 
-;; Macro queries:   http://www.emacswiki.org/emacs/KeyboardMacros#toc5
-(defun my-macro-query (arg)
+
+;; Defined functions
+
+(defun my-macro-query (arg) ; http://www.emacswiki.org/emacs/KeyboardMacros#toc5
   "Prompt for input using minibuffer during kbd macro execution.
     With prefix argument, allows you to select what prompt string to use.
     If the input is non-empty, it is inserted at point."
@@ -76,12 +80,13 @@
 		  (read-from-minibuffer prompt))))
     (unless (string= "" input) (insert input))))
 
-;; Google Search
+
 (defun make-search (base-url)
   (lexical-let ((url base-url))
   (lambda (query)
     (interactive "sSearch:")
     (browse-url-firefox (format url query)))))
+
 
 (defun make-browse (base-url)
   (lexical-let ((url base-url))
@@ -90,13 +95,6 @@
     (browse-url-firefox url))))
 
 
-(global-set-key (kbd "C-c b k") (make-browse "https://duckduckgo.com"))
-(global-set-key (kbd "C-c b g") (make-search "https://www.google.com/search?q=%s"))
-(global-set-key (kbd "<f11>") (make-search "https://www.google.com/search?q=%s"))
-
-
-
-;; Defined functions
 (defun invert-capitalization ()
   "This is a test"
   (interactive)
@@ -114,51 +112,23 @@
   )
 
 
-;; Global editing commands
-
-(global-set-key (kbd "<f9>") 'local-set-key)
-(global-set-key (kbd "C-<f9>") 'global-set-key)
-(global-set-key (kbd "M-c") 'invert-capitalization)
-(global-set-key "\C-xQ" 'my-macro-query)
+(defun reload_init_file () 
+  (interactive) 
+  (load-file "~/.emacs.d/init.el"))
 
 
-(window-numbering-mode)
-(require 'move-text)
-(move-text-default-bindings)
+(defun alarm (minutes)
+  "Play preset sound in X minutes."
+  (interactive "sHow may minutes to alarm?")
+  (run-at-time (format "%s min" minutes) nil (lambda ()
+					       (play-sound-file "/path/to/sound/file"))))
 
-
-;; Reloading the init file
-(defun reload_init_file () 0(interactive) (load-file "~/.emacs.d/init.el"))
-(global-set-key (kbd "C-c q w e") 'reload_init_file)
-
-
-;; CSS
-(add-hook 'css-mode-hook 'rainbow-mode) ; i know what you are thinking
-
-
-;; Activating IDO mode
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
-
-
-;; Desktop 
-;(desktop-save-mode 1)
-
-
-;; Backups and auto-saves
-(setq backup-directory-alist 
-      `((".*" . ,"~/.saves")))
-;(setq auto-save-file-name-transforms
-;      `((".*" ,"~/.saves" t)))
-(setq auto-save-default nil) ; I save too often anyway.
 
 
 ;; Emacs Tranparency: http://www.emacswiki.org/emacs/TransparentEmacs
 ;;(set-frame-parameter (selected-frame) 'alpha '(<active> [<inactive>]))
 (set-frame-parameter (selected-frame) 'alpha '(100 100))
 (add-to-list 'default-frame-alist '(alpha 100 100))
-
 
 (eval-when-compile (require 'cl))
 (defun toggle-transparency ()
@@ -168,18 +138,39 @@
        100)
       (set-frame-parameter nil 'alpha '(100 100))
     (set-frame-parameter nil 'alpha '(85 50))))
+
+
+;; Global editing commands
+
 (global-set-key (kbd "C-c t") 'toggle-transparency)
 
+(global-set-key (kbd "C-c b k") (make-browse "https://duckduckgo.com"))
+(global-set-key (kbd "C-c b g") (make-search "https://www.google.com/search?q=%s"))
+(global-set-key (kbd "<f11>") (make-search "https://www.google.com/search?q=%s"))
 
+(global-set-key (kbd "<f9>") 'local-set-key)
+(global-set-key (kbd "C-<f9>") 'global-set-key)
+
+(global-set-key (kbd "M-c") 'invert-capitalization)
+(global-set-key (kbd "C-x Q") 'my-macro-query)
+(global-set-key (kbd "C-c q w e") 'reload_init_file)
+
+(global-set-key (kbd "C-<f1>") (lambda () (find-file "~/.emacs.d/init.el")))
+
+
+;; CSS
+(add-hook 'css-mode-hook 'rainbow-mode) ; i know what you are thinking
 
 ;; GOLANG
 (require 'go-mode)
 
+;; Emacs lisp
+(add-hook 'emacs-lisp-mode-hook 'flyspell-prog-mode)
 
-;; (defun alarm (minutes)
-;;   "Play preset sound in X minutes."
-;;   (interactive "sHow may minutes to alarm?")
-;;   (run-at-time (format "%s min" minutes) nil (lambda ()
-;; 					       (play-sound-file "/path/to/sound/file"))))
+;; TEXT
+(add-hook 'text-mode-hook 'flyspell-mode)
+(add-hook 'text-mode-hook 'auto-fill-mode)
 
 
+;; Loading init files
+(load "~/.emacs.d/init_python.el")
