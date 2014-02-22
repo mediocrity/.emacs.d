c3321cad1273f1def83f1d55e87aea01dba7b278
mediocrity <kristiansson.tobias@gmail.com>
2014-02-22 22:17:05 +0100

    Cleaned up init_external.el

--- init_external.el
+++ init_external.el
@@ -1,21 +1,17 @@
 ;; External package settings
 
-;; Melpa
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (package-initialize)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-  )
+
+;; Package Archives
+(package-initialize)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 
 (window-numbering-mode)
 
-(require 'move-text)
+
 (move-text-default-bindings)
 
+
 ;; yasnippet
-(add-to-list 'load-path
-	     "~/.emacs.d/elpa/yasnippet-20140106.1009")
-(require 'yasnippet)
 (yas-global-mode 1)
 (setq yas/indent-line nil)
