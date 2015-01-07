8930d7bf0565ebc99619ecbb1b56b7ae754c0ca8
mediocrity <kristiansson.tobias@gmail.com>
2015-01-07 23:47:02 +0100

    Less awful flymake error faces.

--- init_custom.el
+++ init_custom.el
@@ -12,3 +12,11 @@
  '(preview-gs-options
    (quote
     ("-q" "-dNOPAUSE" "-DNOPLATFONTS" "-dPrinted" "-dTextAlphaBits=4" "-dGraphicsAlphaBits=4"))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(flymake-errline ((t (:underline (:color "plum" :style wave)))))
+ '(flymake-warnline ((t (:underline (:color "chartreuse" :style wave))))))
